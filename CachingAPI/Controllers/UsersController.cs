938278f6ba9e4284f83f4bc6b5d0675e4bde9e3e
Cootz <artem2004799@gmail.com>
2023-12-06 22:56:41 +0300

    Add api methods for `UserController`

--- CachingAPI/Controllers/UsersController.cs
+++ CachingAPI/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CachingAPI.Implementation;
+using CachingAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CachingAPI.Controllers
 {
@@ -6,6 +8,14 @@ namespace CachingAPI.Controllers
     [Route("[controller]")]
     public class UsersController : ControllerBase
     {
+        private readonly UsersProvider _usersProvider;
 
+        public UsersController(UsersProvider usersProvider) => _usersProvider = usersProvider;
+
+        [HttpGet]
+        public async Task<ActionResult<User[]>> GetAllUsersAsync() => Ok(await _usersProvider.GetAllAsync());
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUserByIdAsync(int id) => Ok(await _usersProvider.GetUserByIdAsync(id));
     }
 }
