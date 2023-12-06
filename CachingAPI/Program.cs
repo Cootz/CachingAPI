d9061b51d2e5e2981a37134b3b8d5e974664adef
Cootz <artem2004799@gmail.com>
2023-12-06 23:50:02 +0300

    Add swagger. Configure DI.

--- CachingAPI/Program.cs
+++ CachingAPI/Program.cs
@@ -1,14 +1,25 @@
 using CachingAPI.Implementation;
+using CachingAPI.Implementation.Clients;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
+    .AddHttpClient()
+    .AddScoped<JsonPlaceholderClient>()
+    .AddScoped<UsersProvider>()
+    .AddSwaggerGen()
     .AddDbContext<CachingApiDbContext>(
         options => options.UseNpgsql(builder.Configuration.GetConnectionString("CachingApiDatabase")))
     .AddControllers();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 using (var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
 {
